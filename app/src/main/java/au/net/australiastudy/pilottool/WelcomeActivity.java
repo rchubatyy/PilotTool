47762da5dd46f139e481c2709171473e07da0527
Roman <rchubatyy@gmail.com>
2020-06-07 20:54:28 +1000

    Added welcome screen.

---
+++ app/src/main/java/au/net/australiastudy/pilottool/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package au.net.australiastudy.pilottool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button btn = findViewById(R.id.startBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
